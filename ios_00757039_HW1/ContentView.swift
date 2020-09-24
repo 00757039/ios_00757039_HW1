3eb287e0891102dd9b1b3b69f2eceaccfb6191a2
jubao <tqy_4896@hotmail.com>
2020-09-24 20:52:33 +0800

    Initial Commit

---
+++ ios_00757039_HW1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_00757039_HW1
+//
+//  Created by User16 on 2020/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios_00757039_HW1/ios_00757039_HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_00757039_HW1App.swift
+//  ios_00757039_HW1
+//
+//  Created by User16 on 2020/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_00757039_HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
